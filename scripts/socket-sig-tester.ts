a6c8a38b509405c9934f7c45412b54a09be45198
Daniel Bui <dnlbui@proton.me>
2023-11-14 17:10:33 -0600

    add test scripts

---
+++ scripts/socket-sig-tester.ts
@@ -0,0 +1,49 @@
+// This script is used to test the WebSocket connection to a server.
+// It simulates a client sending signed data to the server and logs the server's responses.
+
+import * as crypto from '@shardus/crypto-utils'
+import * as WebSocket from 'ws'
+
+// Initialize the cryptographic utility library with a specific key.
+crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
+
+// Define a developer account with a public key and secret key.
+const devAccount = {
+  publicKey: 'YOUR_PUBLIC_KEY_HERE',
+  secretKey: 'YOUR_SECRET_KEY_HERE',
+}
+
+// Define the data to be sent to the server.
+const data: any = {
+  count: 100,
+  sender: devAccount.publicKey,
+  collectorInfo: {
+    publicKey: '', // Check the collector's config file for the public key
+    secretKey: // Check the collector's config file for the secret key
+      '',
+  },
+}
+
+// Sign the data with the developer account's secret key and public key.
+crypto.signObj(data, devAccount.secretKey, devAccount.publicKey)
+
+// Convert the data to a URL-encoded JSON string.
+const dataString = encodeURIComponent(JSON.stringify(data))
+
+// Open a WebSocket connection to the server and include the data as a query parameter in the URL.
+const ws = new WebSocket(`ws://127.0.0.1:6000?data=${dataString}`)
+
+// Log when the WebSocket connection is opened.
+ws.on('open', function open() {
+  console.log('WebSocket connection opened')
+})
+
+// Log any data received from the server.
+ws.on('message', function incoming(data) {
+  console.log(`Received: ${data}`)
+})
+
+// Log any errors with the WebSocket connection.
+ws.on('error', function error(err) {
+  console.log(`WebSocket error: ${err}`)
+})
--- scripts/tx-endpoint-tester.ts
+++ scripts/tx-endpoint-tester.ts
@@ -1,24 +1,30 @@
 import * as crypto from '@shardus/crypto-utils'
 import fetch from 'node-fetch'
 
-//run by entering command "ts-node api-tester.ts" in terminal
-
+// Initialize the crypto module with a specific seed
 crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
 
+// Define a developer account object with public and secret keys
 const devAccount = {
   publicKey: 'YOUR_PUBLIC_KEY_HERE',
   secretKey: 'YOUR_SECRET_KEY_HERE',
 }
 
+// Data object to be sent in the transaction
 const data: any = {
-  count: 100,
+  start: 0, // start cycle
+  end: 10, // end cycle
+  txId: '',
+  accountId: '',
   sender: devAccount.publicKey,
 }
-crypto.signObj(data, devAccount.secretKey, devAccount.publicKey)
-console.log(data)
 
-// example endpoints... totalData or cycleinfo
-fetch('http://127.0.0.1:6000/cycleinfo', {
+// Signing the data object with the developer's secret and public keys
+crypto.signObj(data, devAccount.secretKey, devAccount.publicKey)
+console.log(data) // Log the signed data object
+
+// Sending a POST request to the specified URL with the transaction data
+fetch('http://127.0.0.1:6000/transaction', {
   method: 'post',
   body: JSON.stringify(data),
   headers: { 'Content-Type': 'application/json' },
